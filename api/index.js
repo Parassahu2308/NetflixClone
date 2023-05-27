6538758730d28e4b373193d637d482603e3645d6
Parassahu2308 <sahujuparas786@gmail.com>
2023-05-27 09:38:43 +0530

    CRUD Api Created

--- api/index.js
+++ api/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// const authRoute = require("./routes/auth");
-// const userRoute = require("./routes/users");
-// const movieRoute = require("./routes/movies");
-// const listRoute = require("./routes/lists");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -21,10 +21,10 @@ mongoose
 
 app.use(express.json());
 
-// app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/movies", movieRoute);
-// app.use("/api/lists", listRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
