d5a1f4960c16341f7133d9cfc39947281fcb2457
Parassahu2308 <sahujuparas786@gmail.com>
2023-05-27 17:52:31 +0530

    movies fetch

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
@@ -20,6 +21,7 @@ mongoose
   });
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
-// import Register from "./pages/register/Register";
-// import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
-  return <Login />;
+  const user = true;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
